1685a5b338cd2ff14048205b0a5901fb34aaf467
Xiao X <103391432+QXIoa@users.noreply.github.com>
2026-05-28 07:46:28 -0400

    maak de j0-functie aan

--- src/de/caydenno1/xacrypto/zekerrijndael/GCM/GCM.java
+++ src/de/caydenno1/xacrypto/zekerrijndael/GCM/GCM.java
@@ -9,4 +9,10 @@ public class GCM {
         byte[] H = cip.encryptBlock(new byte[16]);
         this.gh = new GHASH(H);
     }
+    private static byte[] j0(byte[] iv) {
+        byte[] J0 = new byte[16];
+        System.arraycopy(iv, 0, J0, 0, 12);
+        J0[15] = 0x01;
+        return J0;
+    }
 }
