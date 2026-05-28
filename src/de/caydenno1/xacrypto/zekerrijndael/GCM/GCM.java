b2debc97930ffcfec4922190c5fe1fd1a8635215
Xiao X <103391432+QXIoa@users.noreply.github.com>
2026-05-28 07:54:07 -0400

    maak een inc32-functie aan

--- src/de/caydenno1/xacrypto/zekerrijndael/GCM/GCM.java
+++ src/de/caydenno1/xacrypto/zekerrijndael/GCM/GCM.java
@@ -15,4 +15,15 @@ public class GCM {
         J0[15] = 0x01;
         return J0;
     }
+    private byte[] tag(byte[] aad, byte[] ct, byte[] J0) {
+        byte[] S   = gh.compute(aad, ct);
+        byte[] EJ0 = cip.encryptBlock(J0);
+        for (int i = 0; i < 16; i++) S[i] ^= EJ0[i];
+        return S;
+    }
+    private static byte[] inc32(byte[] b) {
+        byte[] o = Arrays.copyof(b,16);
+        for (int i = 15; i >= 12; i--) if (++out[i] != 0) break;
+        return o;
+    }
 }
