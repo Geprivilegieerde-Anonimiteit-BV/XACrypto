6aadf81988387d252cff2546ef872f64d18a18dd
Xiao X <xiao@mathai.cloud>
2026-05-29 18:03:35 -0400

    AES.java potential

---
+++ src/de/caydenno1/xacrypto/zekerrijndael/GCM/AES.java
@@ -0,0 +1,216 @@
+package de.caydenno1.xacrypto.zekerrijndael.GCM;
+
+import de.caydenno1.xacrypto.misc.XACryptoException;
+
+import java.util.Arrays;
+
+import static de.caydenno1.xacrypto.zekerrijndael.UnchangingData.*;
+
+public final class AES implements BlockCipher {
+    public final int bits;
+    public int kc;
+    private final byte[][] keys;
+
+    public AES(byte[] key, int bits) throws XACryptoException {
+        this.bits = bits;
+        this.kc = switch (bits) {
+            case 128 -> 44;
+            case 192 -> 52;
+            case 256 -> 60;
+            default -> throw new XACryptoException(
+                    "only 128, 192 and 256 bit ciphers are supported atm. sorry :%"
+            );
+        };
+        this.keys = new byte[kc][4];
+
+        keyExpansion(key);
+    }
+
+    public byte[] encryptBlock(byte[] input) throws XACryptoException {
+        int m = switch (bits) {
+            case 128 -> 10;
+            case 192 -> 12;
+            case 256 -> 14;
+            default -> throw new XACryptoException("only 128, 192 and 256 bit ciphers are supported atm. sorry :%");
+        };
+
+        byte[][] s = new byte[4][4];
+
+        for (int i = 0; i < m; i++) {
+            s[i & 3][i >> 2] = input[i];
+        }
+
+        addRoundKey(s, m);
+
+        for (int r = 1; r < 10; r++) {
+            sub(s);
+            shift(s);
+            mixColumns(s);
+            addRoundKey(s, r);
+        }
+
+        sub(s);
+        shift(s);
+        addRoundKey(s, 10);
+
+        byte[] o = new byte[16];
+
+        for (int i = 0; i < 16; i++) {
+            o[i] = s[i & 3][i >> 2];
+        }
+
+        return o;
+    }
+
+    private void sub(byte[][] s) {
+        for (int c = 0; c < 4; c++) {
+            for (int r = 0; r < 4; r++) {
+                s[r][c] = SBOX[s[r][c] & 0xFF];
+            }
+        }
+    }
+
+    private void shift(byte[][] s) {
+
+        byte t;
+
+        t = s[1][0];
+        s[1][0] = s[1][1];
+        s[1][1] = s[1][2];
+        s[1][2] = s[1][3];
+        s[1][3] = t;
+
+        t = s[2][0];
+        byte t2 = s[2][1];
+        s[2][0] = s[2][2];
+        s[2][1] = s[2][3];
+        s[2][2] = t;
+        s[2][3] = t2;
+
+        t = s[3][3];
+        s[3][3] = s[3][2];
+        s[3][2] = s[3][1];
+        s[3][1] = s[3][0];
+        s[3][0] = t;
+    }
+
+    private void mixColumns(byte[][] s) {
+
+        for (int c = 0; c < 4; c++) {
+
+            byte a0 = s[0][c];
+            byte a1 = s[1][c];
+            byte a2 = s[2][c];
+            byte a3 = s[3][c];
+
+            s[0][c] = (byte)(gm2(a0) ^ gm3(a1) ^ a2 ^ a3);
+            s[1][c] = (byte)(a0 ^ gm2(a1) ^ gm3(a2) ^ a3);
+            s[2][c] = (byte)(a0 ^ a1 ^ gm2(a2) ^ gm3(a3));
+            s[3][c] = (byte)(gm3(a0) ^ a1 ^ a2 ^ gm2(a3));
+        }
+    }
+
+    private byte gm2(byte b) {
+        int x = b & 0xFF;
+        return (byte)(((x << 1) ^ ((x & 0x80) != 0 ? 0x1b : 0)) & 0xFF);
+    }
+
+    private byte gm3(byte b) {
+        return (byte)(gm2(b) ^ b);
+    }
+
+    private void addRoundKey(byte[][] s, int round) {
+        for (int c = 0; c < 4; c++) {
+            for (int r = 0; r < 4; r++) {
+                s[r][c] ^= keys[round * 4 + c][r];
+            }
+        }
+    }
+
+    private void keyExpansion(byte[] key) throws XACryptoException {
+        int tem = switch(bits) {
+            case 128 -> 4;
+            case 192 -> 6;
+            case 256 -> 8;
+            default -> throw new XACryptoException(
+                    "only 128, 192 and 256 bit ciphers are supported atm. sorry :%"
+            );
+        };
+        for (int i = 0; i < tem; i++) {
+            System.arraycopy(key, i * 4, keys[i], 0, 4);
+        }
+
+        for (int i = 4; i < kc; i++) {
+
+            byte[] temp = Arrays.copyOf(keys[i - 1], 4);
+
+            if (i % tem == 0 && bits != 256) {
+
+                byte t = temp[0];
+                temp[0] = temp[1];
+                temp[1] = temp[2];
+                temp[2] = temp[3];
+                temp[3] = t;
+
+                for (int j = 0; j < 4; j++) {
+                    temp[j] = SBOX[temp[j] & 0xFF];
+                }
+
+                temp[0] ^= (byte)(RCON[i / 4] >>> 24);
+            }
+
+            if (bits == 256 && i % 8 == 4) for (int j = 0 ; j < 4 ; j++) temp[j] = SBOX[temp[j] & 0xFF];
+
+            for (int j = 0; j < 4; j++) keys[i][j] = (byte)(keys[i - 4][j] ^ temp[j]);
+        }
+    }
+
+    public byte[] encryptCBC(byte[] pln, byte[] iv) throws XACryptoException {
+
+        byte[] o = new byte[pln.length];
+        byte[] prev = Arrays.copyOf(iv, 16);
+
+        byte[] block = new byte[16];
+
+        for (int i = 0; i < pln.length; i += 16) {
+
+            for (int j = 0; j < 16; j++) {
+                block[j] = (byte)(pln[i + j] ^ prev[j]);
+            }
+
+            byte[] enc = encryptBlock(block);
+
+            System.arraycopy(enc, 0, o, i, 16);
+
+            prev = enc;
+        }
+
+        return o;
+    }
+
+    public byte[] encryptCTR(byte[] pln, byte[] nonce) throws XACryptoException {
+
+        byte[] o = new byte[pln.length];
+
+        byte[] cnt = Arrays.copyOf(nonce, 16);
+        byte[] ks;
+
+        for (int i = 0; i < pln.length; i += 16) {
+
+            int ctr = i >>> 4;
+
+            cnt[12] = (byte)(ctr >>> 24);
+            cnt[13] = (byte)(ctr >>> 16);
+            cnt[14] = (byte)(ctr >>> 8);
+            cnt[15] = (byte)(ctr);
+
+            ks = encryptBlock(cnt);
+
+            for (int j = 0; j < 16; j++) {
+                o[i + j] = (byte) (pln[i + j] ^ ks[j]);
+            }
+        }
+
+        return o;
+    }
+}
--- src/de/caydenno1/xacrypto/zekerrijndael/GCM/BlockCipher.java
+++ src/de/caydenno1/xacrypto/zekerrijndael/GCM/BlockCipher.java
@@ -1,5 +1,7 @@
 package de.caydenno1.xacrypto.zekerrijndael.GCM;
 
+import de.caydenno1.xacrypto.misc.XACryptoException;
+
 public interface BlockCipher {
-    byte[] encryptBlock(byte[] pln);
+    byte[] encryptBlock(byte[] pln) throws XACryptoException;
 }
