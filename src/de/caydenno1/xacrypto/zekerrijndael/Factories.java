5fd7703e5c02f0614bdf8f657f817440b1a81a9b
Xiao X <xiao@mathai.cloud>
2026-05-29 11:45:14 -0400

    add factory

--- src/de/caydenno1/xacrypto/zekerrijndael/Factories.java
+++ src/de/caydenno1/xacrypto/zekerrijndael/Factories.java
@@ -4,12 +4,11 @@ import de.caydenno1.xacrypto.misc.XACryptoException;
 import de.caydenno1.xacrypto.zekerrijndael.GCM.AES128;
 import de.caydenno1.xacrypto.zekerrijndael.GCM.GCM;
 import de.caydenno1.xacrypto.zekerrijndael.GCM.ciphers.SM4GCM;
-
+import de.caydenno1.xacrypto.zekerrijndael.GCM.ciphers.Camellia;
 public class Factories {
     public static GCM AES128(byte[] key) {
         return new GCM(new AES128(key));
     }
-    public static GCM SM4GCM(byte[] key) throws XACryptoException {
-        return new GCM(new SM4GCM(key));
-    }
+    public static GCM SM4GCM(byte[] key) throws XACryptoException { return new GCM(new SM4GCM(key));}
+    public static GCM Camellia(byte[] key) throws XACryptoException { return new GCM(new Camellia(key));}
 }
