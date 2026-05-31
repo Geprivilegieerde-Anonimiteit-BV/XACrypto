4e08c5335f3e2a25adf1c8252fc6ab2881208175
Xiao X <xiao@mathai.cloud>
2026-05-30 21:06:16 -0400

    Aria.java complete!

--- src/de/caydenno1/xacrypto/zekerrijndael/UnchangingData.java
+++ src/de/caydenno1/xacrypto/zekerrijndael/UnchangingData.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fe604618f16385149487389f64a2435f0d8fbc0717c6c30ba2abbb8b2ea65ef
-size 25225
+oid sha256:d664cda1dcd9b2ad5f583094a88e99dd0c590ee4003168fafc684c4ee81010b1
+size 26020
