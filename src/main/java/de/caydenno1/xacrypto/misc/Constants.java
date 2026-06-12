61d52f088b2e770772f8618e6c4185261bce5ed4
Xiao X <xiao@mathai.cloud>
2026-06-12 17:51:49 -0400

    SHA384 is working
    LESGO!

--- src/main/java/de/caydenno1/xacrypto/misc/Constants.java
+++ src/main/java/de/caydenno1/xacrypto/misc/Constants.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f4f9d90a3ee0f6a1e37131e6a8056cf46b02e6c8f517a592fb0c06870bfca65
-size 2765
+oid sha256:8578786b00f67cea6476c9e4ab7de2432344bbfb951c413e06824636712c3f12
+size 4978
