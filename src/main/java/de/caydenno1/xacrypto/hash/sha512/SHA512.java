4eabb60ba75f3a5f87ea1fbc85cc62a23f39b0f3
Xiao X <xiao@mathai.cloud>
2026-06-12 17:58:31 -0400

    Create SHA512.java

---
+++ src/main/java/de/caydenno1/xacrypto/hash/sha512/SHA512.java
@@ -0,0 +1,6 @@
+package de.caydenno1.xacrypto.hash.sha512;
+
+public class SHA512 {
+    // i don't have the time right now to work on this. this will be finished in the near (soon to come) future
+    // it is possible another dev (such as tiendaXD) will work on this in the meantime.
+}
