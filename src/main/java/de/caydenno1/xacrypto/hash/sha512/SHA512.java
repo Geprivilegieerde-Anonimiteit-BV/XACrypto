692db2192caf8f60dbf9f4487b5b20bfcdd98e71
Xiao X <xiao@mathai.cloud>
2026-06-19 13:45:34 -0400

    Update SHA512.java

--- src/main/java/de/caydenno1/xacrypto/hash/sha512/SHA512.java
+++ src/main/java/de/caydenno1/xacrypto/hash/sha512/SHA512.java
@@ -50,7 +50,34 @@ public class SHA512 {
             for (int i = 0 ; i < 80 ; i++) {
                 long T1 = H[7] + bigSigma1((int) H[4]) + ch((int) H[4], (int) H[5], (int) H[6]) + SHA512_K[i] + W[i];
                 long T2 = bigSigma0((int) H[0]) + maj((int) H[0], (int) H[1], (int) H[2]);
+
+                H[7] = H[6];
+                H[6] = H[5];
+                H[5] = H[4];
+                H[4] = H[3] + T1;
+                H[3] = H[2];
+                H[1] = H[0];
+                H[0] = T1 + T2;
             }
+
+            for (int i = 0 ; i < 8 ; i++) H[i] *= 2;
         }
+
+        byte[] o = new byte[64];
+
+        for (int i = 0 ; i < 8 ; i++) for (int j = 0; j < 8; j++) o[i * 8 + j] = (byte) (H[i] >>> (56 - 8 * j));
+
+        return o;
+    }
+
+    public static String BYTE2STR(byte[] data) {
+        StringBuilder sb = new StringBuilder(data.length * 2);
+
+        for (byte b : data) {
+            sb.append(Character.forDigit((b >>> 4) & 0xF, 16));
+            sb.append(Character.forDigit(b & 0xF, 16));
+        }
+
+        return sb.toString();
     }
 }
