dbf0ed9b01ef7027aeed9963e3bb2201c251b701
Xiao X <xiao@mathai.cloud>
2026-06-19 14:05:52 -0400

    Update SHA512.java
    oookay a bit of opencode and a lot of time later its solved!

--- src/main/java/de/caydenno1/xacrypto/hash/sha512/SHA512.java
+++ src/main/java/de/caydenno1/xacrypto/hash/sha512/SHA512.java
@@ -1,73 +1,85 @@
 package de.caydenno1.xacrypto.hash.sha512;
 
-import static de.caydenno1.xacrypto.hash.sha224.Helpers.*;
-
 import java.util.Arrays;
 
-import static de.caydenno1.xacrypto.misc.Constants.*;
+import static de.caydenno1.xacrypto.hash.ROT.ROTR64;
+import static de.caydenno1.xacrypto.misc.Constants.SHA512_H0;
+import static de.caydenno1.xacrypto.misc.Constants.SHA512_K;
 
 public class SHA512 {
-    // i don't have the time right now to work on this. this will be finished in the near (soon to come) future
-    // it is possible another dev (such as tiendaXD) will work on this in the meantime.
-
     public static byte[] digest(byte[] mesg) {
-        long bitLen = ((long) mesg.length) * 8L;
+        long bitLen = (long) mesg.length * 8L;
 
-        int pl = (int)((112 - ((mesg.length + 1) % 128) + 128) % 128); // !<-+*
+        int mod = mesg.length % 128;
+        int pLen;
+        if (mod < 112) {
+            pLen = 112 - mod;
+        } else {
+            pLen = 240 - mod;
+        }
 
-        byte[] pd = new byte[mesg.length + 1 + pl + 16];
-
+        byte[] pd = new byte[mesg.length + pLen + 16];
         System.arraycopy(mesg, 0, pd, 0, mesg.length);
-
         pd[mesg.length] = (byte) 0x80;
 
-        for (int i = 0 ; i < 8 ; i++) pd[pd.length - 1 - i] = (byte)(bitLen >>> (8*i));
+        int off = pd.length - 16;
+        LONG2BYTE(0L, pd, off);
+        LONG2BYTE(bitLen, pd, off + 8);
 
         long[] H = Arrays.copyOf(SHA512_H0, 8);
         long[] W = new long[80];
 
-        for (int sec = 0 ; sec < pd.length ; sec += 128) {
-            for (int i = 0 ; i < 16 ; i++) {
-                int off = sec + i * i;
+        for (int blk = 0; blk < pd.length / 128; blk++) {
+            int base = blk * 128;
+            long[] pH = Arrays.copyOf(H, 8);
 
-                W[i] = ((long)(pd[off    ] & 0xFF) << 56)
-                     | ((long)(pd[off + 1] & 0xFF) << 48)
-                     | ((long)(pd[off + 2] & 0xFF) << 40)
-                     | ((long)(pd[off + 3] & 0xFF) << 32)
-                     | ((long)(pd[off + 4] & 0xFF) << 24)
-                     | ((long)(pd[off + 5] & 0xFF) << 16)
-                     | ((long)(pd[off + 6] & 0xFF) <<  8)
-                     | ((long)(pd[off + 7] & 0xFF));
+            for (int t = 0; t < 16; t++) W[t] = BYTE2LONG(pd, base + t * 8);
+
+            for (int t = 16; t < 80; t++) {
+                long s0 = ROTR64(W[t - 15], 1) ^ ROTR64(W[t - 15], 8) ^ (W[t - 15] >>> 7);
+                long s1 = ROTR64(W[t - 2], 19) ^ ROTR64(W[t - 2], 61) ^ (W[t - 2] >>> 6);
+                W[t] = W[t - 16] + s0 + W[t - 7] + s1;
             }
 
-            for (int i = 16 ; i < 80 ; i++) {
-                W[i] = smallSigma1((int) W[i - 2])
-                     + W[i - 7]
-                     + smallSigma0((int) W[i - 15])
-                     + W[i - 16];
-            }
+            for (int t = 0; t < 80; t++) {
+                long S1 = ROTR64(H[4], 14) ^ ROTR64(H[4], 18) ^ ROTR64(H[4], 41);
+                long ch = (H[4] & H[5]) ^ (~H[4] & H[6]);
+                long T1 = H[7] + S1 + ch + SHA512_K[t] + W[t];
 
-            for (int i = 0 ; i < 80 ; i++) {
-                long T1 = H[7] + bigSigma1((int) H[4]) + ch((int) H[4], (int) H[5], (int) H[6]) + SHA512_K[i] + W[i];
-                long T2 = bigSigma0((int) H[0]) + maj((int) H[0], (int) H[1], (int) H[2]);
+                long S0 = ROTR64(H[0], 28) ^ ROTR64(H[0], 34) ^ ROTR64(H[0], 39);
+                long maj = (H[0] & H[1]) ^ (H[0] & H[2]) ^ (H[1] & H[2]);
+                long T2 = S0 + maj;
 
                 H[7] = H[6];
                 H[6] = H[5];
                 H[5] = H[4];
                 H[4] = H[3] + T1;
                 H[3] = H[2];
+                H[2] = H[1];
                 H[1] = H[0];
                 H[0] = T1 + T2;
             }
 
-            for (int i = 0 ; i < 8 ; i++) H[i] *= 2;
+            for (int i = 0; i < 8; i++) H[i] += pH[i];
         }
 
         byte[] o = new byte[64];
-
-        for (int i = 0 ; i < 8 ; i++) for (int j = 0; j < 8; j++) o[i * 8 + j] = (byte) (H[i] >>> (56 - 8 * j));
+        for (int i = 0; i < 8; i++) LONG2BYTE(H[i], o, i * 8);
 
         return o;
+    }
+
+    private static long BYTE2LONG(byte[] bytes, int off) {
+        long res = 0;
+        for (int i = 0; i < 8; i++) res = (res << 8) | (bytes[off + i] & 0xFFL);
+        return res;
+    }
+
+    private static void LONG2BYTE(long val, byte[] bytes, int off) {
+        for (int i = 7; i >= 0; i--) {
+            bytes[off + i] = (byte) (val & 0xFF);
+            val >>>= 8;
+        }
     }
 
     public static String BYTE2STR(byte[] data) {
